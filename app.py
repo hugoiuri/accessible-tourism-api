6f0ea324945a7b2313084996b7c2129018f2ee32
Hugo Iuri <hugo@minutrade.com>
2018-07-10 01:46:07 -0300

    ajustes para deploy no heroku

--- app.py
+++ app.py
@@ -3,10 +3,10 @@
 import os
 import api
 
+APP = api.create_app()
+APP.debug = True
+HOST = os.environ.get('IP', '0.0.0.0')
+PORT = int(os.environ.get('PORT', 8000))
 
 if __name__ == '__main__':
-    APP = api.create_app()
-    APP.debug = True
-    HOST = os.environ.get('IP', '0.0.0.0')
-    PORT = int(os.environ.get('PORT', 8080))
     APP.run(host=HOST, port=PORT)
